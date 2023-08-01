76c63da99c330d7a82162be4d9d6d727b0f2ed18
szhao23 <stevenzhao456@gmai.com>
2023-08-01 13:42:56 -0500

    Added TradingView API, added CSS Styling, and JS code to display TradingView Chart

--- code/src/index.js
+++ code/src/index.js
@@ -1,1 +1,23 @@
 // Declare Variables
+new TradingView.widget({
+  autosize: true,
+  symbol: "BINANCE:BTCUSDT",
+  interval: "240",
+  timezone: "Etc/Utc",
+  theme: "light",
+  style: "1",
+  locale: "en",
+  toolbar_bg: "#f1f3f6",
+  enable_publishing: false,
+  withdateranges: true,
+  hide_side_toolbar: false,
+  allow_symbol_change: true,
+  watchlist: ["BINANCE:BTCUSDT", "BINANCE:ETHUSDT"],
+  details: true,
+  hotlist: true,
+  calendar: true,
+  studies: ["STD;SMA"],
+  container_id: "chart",
+  show_popup_button: true,
+  popup_width: "1000",
+});
